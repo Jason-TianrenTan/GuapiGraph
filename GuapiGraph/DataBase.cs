93e128b239c88fb31237bf6bc5e8a564b32c54d5
Chidori <13522330013@163.com>
2017-12-22 15:09:52 +0800

    initial commit

---
+++ GuapiGraph/DataBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuapiGraph
+{
+    class DataBase : DataReader
+    {
+        /*
+         * 每个月份的岗位个数
+         */
+        private static Dictionary<int, int> monthCount = new Dictionary<int, int>();
+
+
+        /*
+         * Java/Python/C++
+         */
+        private static Dictionary<String, int> skillCount = new Dictionary<string, int>();
+
+        /*
+         *  运维/前端/分布式
+         */
+        private static Dictionary<String, int> positionCount = new Dictionary<string, int>();
+
+        
+        /*
+         * 实例化接口
+         */
+        void DataReader.readData()
+        {
+
+        }
+    }
+}
---
+++ GuapiGraph/JobBean.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuapiGraph
+{
+    class JobBean
+    {
+        String position; //运维 前端 后台
+        String skill; //java/unix
+        List<string> adjList; //形容词
+        List
+    }
+}
